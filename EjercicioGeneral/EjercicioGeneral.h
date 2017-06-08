e15c55439f677541e3355bc11750db55ff765d2b
jesus2708 <calderon3356@hotmail.com>
2017-06-08 10:07:59 -0600

    ejercicio 6

--- EjercicioGeneral/EjercicioGeneral.h
+++ EjercicioGeneral/EjercicioGeneral.h
@@ -1,5 +1,6 @@
 #pragma once  //______________________________________ EjercicioGeneral.h  
 #include "Resource.h"
+#include "Ejercico6Dlg.h"
 class EjercicioGeneral : public Win::Dialog
 {
 public:
